a5d82735d05ece9365e0c634eebcd5ed00738a4f
slleclipse <1292517143@qq.com>
2019-03-26 09:07:05 +0800

    定时器和非缓存channel

---
+++ gochannel/main/4_17timerbase.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	timer := time.NewTimer(time.Second * 2)
+	fmt.Printf("Present time: %v.\n", time.Now())
+	expirationTime := <- timer.C
+	fmt.Printf("Expiration time: %v.\n", expirationTime)
+	fmt.Printf("Stop timer: %v.\n", timer.Stop())
+}
